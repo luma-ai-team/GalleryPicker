12c933270178e3fcc3ffe298912910f1b84fa737
Luma <admin@luma.ai>
2023-10-02 11:22:03 +0300

    Updated Flow layout calculations

--- Sources/GalleryPicker/Presentation/Picker/Components/GalleryPickerCollectionViewFlowLayout.swift
+++ Sources/GalleryPicker/Presentation/Picker/Components/GalleryPickerCollectionViewFlowLayout.swift
@@ -31,20 +31,17 @@ public class GalleryPickerFlowLayout: UICollectionViewFlowLayout {
 
 extension GalleryPickerFlowLayout {
     public func getCellSize(for collectionView: UICollectionView) -> CGSize {
-        let contentWidth = collectionView.bounds.width - (sectionInset.left + sectionInset.right)
-
         let minWidth = 320.0 / CGFloat(itemsPerRow)
         let maxWidth = 520.0 / CGFloat(itemsPerRow)
+        
         let referenceCellWidth = minWidth + 0.5 * (maxWidth - minWidth)
+        let contentWidth = collectionView.bounds.width - (sectionInset.left + sectionInset.right)
 
-        let numberOfColumns = round(contentWidth / referenceCellWidth)
-        let totalSpacing = CGFloat(numberOfColumns - 1) * minimumInteritemSpacing
-        let cellWidth = ((contentWidth - totalSpacing) / numberOfColumns)
-        
-        // Make sure cell width is bounded by minWidth and maxWidth
-        let adjustedCellWidth = min(max(cellWidth, minWidth), maxWidth)
-        let cellHeight = adjustedCellWidth * aspectRatio
-        
-       return CGSize(width: adjustedCellWidth, height: cellHeight)
+        let collectionViewWidth = collectionView.bounds.width
+
+        let numberOfColumns = round(collectionViewWidth / referenceCellWidth)
+        let width = ((contentWidth - minimumInteritemSpacing * (numberOfColumns - 1)) / numberOfColumns)
+        let height = width * (1 / aspectRatio)
+        return CGSize(width: width, height: height)
     }
 }
